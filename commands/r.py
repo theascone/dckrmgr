bf0c3dd49b0a756c6e303889b5e7dc9d4e173b32
Josia <josia-internet@rosskopfs.de>
2015-12-07 11:38:46 +0100

    will continue if stop/remove fails (not critical)

--- commands/r.py
+++ commands/r.py
@@ -1,10 +1,15 @@
 from dckrmgr import cli
 from dckrmgr import m_cmd
+import requests
 
 def func(ctx):
     cnf = ctx['cnf']
-    cli.remove_container(container = cnf['name'])
-    print('Removed ' + cnf['name'])
+    try:
+      cli.remove_container(container = cnf['name'])
+      print('Removed ' + cnf['name'])
+    except requests.exceptions.HTTPError:
+      print('Could not remove ' + cnf['name'] + '!')
+
     return 0
 
 m_cmd['r'] = {
--- commands/t.py
+++ commands/t.py
@@ -1,12 +1,13 @@
 from dckrmgr import cli
 from dckrmgr import m_cmd
+import requests
 
 def func(ctx):
     cnf = ctx['cnf']
     try:
       cli.stop(cnf['name'])
       print('Stopped ' + cnf['name'])
-    except HTTPError:
+    except requests.exceptions.HTTPError:
       print('Could not stop ' + cnf['name'] + '!')
     return 0
 
